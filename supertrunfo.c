09122422db2e821932fed4a26c4e3aadf09ed53f
AlensuFdevv <202604082516@alunos.estacio.br>
2026-04-22 12:34:11 +0000

    Adicionei printf no codigo!!

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+ 
+    //Desafio super trunfo - paises
+    //Tema 1 - Cadastro das cartas
 
+    int main() {
+    //Area para definição das variaveis cartas
+
+    //Area para entrada de dados
+
+    //Area para exibição das cartas
+    printf("ola mundo, no codespace! \n");
+
+    
+
+
+
+
+
+
+}
